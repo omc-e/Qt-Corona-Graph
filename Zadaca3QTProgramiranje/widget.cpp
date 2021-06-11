b0d17ef6d55a226a1c4cf45b23fb9f02d8b68396
Emsar <emsar.omic@outlook.com>
2021-06-11 09:21:13 -0700

    Added Scene

--- Zadaca3QTProgramiranje/widget.cpp
+++ Zadaca3QTProgramiranje/widget.cpp
@@ -6,6 +6,13 @@ Widget::Widget(QWidget *parent) :
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    scena = new QGraphicsScene(this);
+    scena->setSceneRect (-10, -280, 20, 80);
+    scena ->setBackgroundBrush(Qt::gray);
+
+    ui->graphicsView->setScene(scena);
+
+    QPen crna_olovka(Qt::black);
 }
 
 Widget::~Widget()
