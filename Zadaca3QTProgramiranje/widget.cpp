228069ea9e8142ca948f6fec25f1ace8b7368d7d
Emsar <emsar.omic@outlook.com>
2021-06-12 17:13:07 -0700

    Dodani podioci na Y osi

--- Zadaca3QTProgramiranje/widget.cpp
+++ Zadaca3QTProgramiranje/widget.cpp
@@ -14,7 +14,12 @@ Widget::Widget(QWidget *parent) :
 
     QPen crna_olovka(Qt::black);
     crna_olovka.setWidth(3);
+    QPainter painter;
+    QPen bijela_olovka(Qt::white);
 
+    painter.setPen(crna_olovka);
+
+    //koordinatni sistem
     xosa=scena->addLine(0,0,385,0,crna_olovka);
     xosa2=scena->addLine(0,0,-385,0,crna_olovka);
     yosa=scena->addLine(0,0,0,370,crna_olovka);
@@ -23,6 +28,21 @@ Widget::Widget(QWidget *parent) :
     strelica2=scena->addLine(350,-20,385,0,crna_olovka);
     strelica3=scena->addLine(0,-525,20,-490,crna_olovka);
     strelica4=scena->addLine(0,-525,-20,-490,crna_olovka);
+
+    //podioci za slucajeve
+    l1=scena->addLine(-5,-50,5,-50, bijela_olovka);
+    l2=scena->addLine(-5,-100,5,-100, bijela_olovka);
+    l3=scena->addLine(-5,-150,5,-150,bijela_olovka);
+    l4=scena->addLine(-5,-200,5,-200,bijela_olovka);
+    l5=scena->addLine(-5,-250,5,-250,bijela_olovka);
+    l6=scena->addLine(-5,-300,5,-300,bijela_olovka);
+    l7=scena->addLine(-5,-350,5,-350,bijela_olovka);
+    l8=scena->addLine(-5,-400,5,-400,bijela_olovka);
+    l9=scena->addLine(-5,-450,5,-450,bijela_olovka);
+    l0=scena->addLine(-5,-500,5,-500,bijela_olovka);
+    l10=scena->addLine(-5,0,-5,0,bijela_olovka);
+
+
 
 }
 
--- Zadaca3QTProgramiranje/widget.h
+++ Zadaca3QTProgramiranje/widget.h
@@ -9,6 +9,8 @@
 #include <QFont>
 #include <QtCore>
 #include <QtGui>
+#include <QStringList>
+#include <QString>
 
 
 namespace Ui {
@@ -34,6 +36,17 @@ private:
     QGraphicsLineItem *strelica2;
     QGraphicsLineItem *strelica3;
     QGraphicsLineItem *strelica4;
+    QGraphicsLineItem *l1;
+    QGraphicsLineItem *l2;
+    QGraphicsLineItem *l3;
+    QGraphicsLineItem *l4;
+    QGraphicsLineItem *l5;
+    QGraphicsLineItem *l6;
+    QGraphicsLineItem *l7;
+    QGraphicsLineItem *l8;
+    QGraphicsLineItem *l9;
+    QGraphicsLineItem *l0;
+    QGraphicsLineItem *l10;
 };
 
 #endif // WIDGET_H
